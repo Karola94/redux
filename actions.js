800a47935f0ce2f4aeb40f3fe804bd168aa2bb21
Karolina Kuchta <linka_94@interia.eu>
2019-11-27 12:11:25 +0100

    Change actions.js

--- actions.js
+++ actions.js
@@ -1,18 +1,21 @@
 import uuid from uuid;
 
+//Obiekt aplikacji
+var appObject = {
+    type,
+    id,
+    text,
+    thumbNumber
+}
+
 //Tworzenie komentarza
 const ADD_COMMENT = 'ADD_COMMENT';
 
-{
-    type: ADD_COMMENT; 
-    text: 'My first comment !'
-}
 function addComment(text) {
-    return {
-        type: ADD_COMMENT,
-        text,
-        id: uuid.v4()
-    }
+    appObject.type = ADD_COMMENT;
+    appObject.text = text;
+    appObject.id = uuid.v4();
+    return appObject;
 }
 
 const boundAddComment = text => dispatch(addComment(text));
@@ -21,17 +24,11 @@ boundAddComment('nowy komentarz!');
 //Edycja komentarza
 const EDIT_COMMENT = 'EDIT_COMMENT';
 
-{
-    type: EDIT_COMMENT;
-    id: 20;
-    text: 'wyedytowany tekst komentarza'
-}
 function editComment(text) {
-    return {
-        type: EDIT_COMMENT,
-        text,
-        id: uuid.v4()
-    }
+    appObject.type = EDIT_COMMENT;
+    appObject.text = text;
+    appObject.id = uuid.v4();
+    return appObject;   
 }
 
 const boundEditComment = text => dispatch(editComment(text));
@@ -39,15 +36,11 @@ boundEditComment('edytowany komentarz!');
 
 //Usuwanie komentarza
 const REMOVE_COMMENT = 'REMOVE_COMMENT';
-{
-    type: REMOVE_COMMENT;
-    id: 20 // przykładowy id komentarza do usunięcia
-}
+
 function removeComment(id) {
-    return {
-        type: REMOVE_COMMENT,
-        id: uuid.v4()
-    }
+    appObject.type = REMOVE_COMMENT;    
+    appObject.id = id;
+    return appObject;  
 }
 
 const boundRemoveComment = id => dispatch(removeComment(id));
@@ -57,32 +50,20 @@ boundRemoveComment(20);
 const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-{
-    type: THUMB_UP_COMMENT;
-    id: 20;
-    thumbNumber: 4
-}
-{
-    type: THUMB_DOWN_COMMENT;
-    id: 5;
-    thumbNumber: 8
-}
 function addThumb(id) {
-    return {
-        type: THUMB_UP_COMMENT,
-        id: uuid.v4(),
-        thumbNumber: 4
-    }
+    appObject.type = THUMB_UP_COMMENT;    
+    appObject.id = id;
+    appObject.thumbNumber +=1;
+    return appObject;   
 }
 const boundAddThumb = id => dispatch(addThumb(id));
 boundAddThumb(20);
 
 function subThumb(id) {
-    return {
-        type: THUMB_DOWN_COMMENT,
-        id: uuid.v4(),
-        thumbNumber: 8
-    }
+    appObject.type = THUMB_DOWN_COMMENT;    
+    appObject.id = id;
+    appObject.thumbNumber -=1;
+    return appObject;  
 }
 const boundSubThumb = id => dispatch(subThumb(id));
 boundSubThumb(5);
