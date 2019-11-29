29255ced13d35bf4130774b01fb8ae56c71b2deb
Karolina Kuchta <linka_94@interia.eu>
2019-11-29 15:20:04 +0100

    Reducers separation

--- actions.js
+++ actions.js
@@ -1,69 +1,67 @@
 import uuid from uuid;
 
-//Obiekt aplikacji
-var appObject = {
-    type,
-    id,
-    text,
-    thumbNumber
-}
-
 //Tworzenie komentarza
 const ADD_COMMENT = 'ADD_COMMENT';
 
 function addComment(text) {
-    appObject.type = ADD_COMMENT;
-    appObject.text = text;
-    appObject.id = uuid.v4();
-    return appObject;
+    return {
+        type: ADD_COMMENT,
+        text: text,
+        id: uuid.v4()
+    }    
 }
 
 const boundAddComment = text => dispatch(addComment(text));
-boundAddComment('nowy komentarz!');
+// boundAddComment('nowy komentarz!');
 
 //Edycja komentarza
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 
 function editComment(text) {
-    appObject.type = EDIT_COMMENT;
-    appObject.text = text;
-    appObject.id = uuid.v4();
-    return appObject;   
+    return {
+        type = EDIT_COMMENT,
+        text = text,
+        id = uuid.v4()
+    }      
 }
 
 const boundEditComment = text => dispatch(editComment(text));
-boundEditComment('edytowany komentarz!');
+//boundEditComment('edytowany komentarz!');
 
 //Usuwanie komentarza
 const REMOVE_COMMENT = 'REMOVE_COMMENT';
 
 function removeComment(id) {
-    appObject.type = REMOVE_COMMENT;    
-    appObject.id = id;
-    return appObject;  
+    return {
+        type = REMOVE_COMMENT,    
+        id = id
+    }
+          
 }
 
 const boundRemoveComment = id => dispatch(removeComment(id));
-boundRemoveComment(20);
+//boundRemoveComment(20);
 
 //Ocenianie +1/-1 komentarza
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-function addThumb(id) {
-    appObject.type = THUMB_UP_COMMENT;    
-    appObject.id = id;
-    appObject.thumbNumber +=1;
-    return appObject;   
+function addThumb(id, thumbNumber) {
+    return {
+        type = THUMB_UP_COMMENT,   
+        id = id,
+        thumbNumber = votes 
+    }        
 }
 const boundAddThumb = id => dispatch(addThumb(id));
-boundAddThumb(20);
+//boundAddThumb(20);
 
-function subThumb(id) {
-    appObject.type = THUMB_DOWN_COMMENT;    
-    appObject.id = id;
-    appObject.thumbNumber -=1;
-    return appObject;  
+function subThumb(id, thumbNumber) {
+    return {
+        type = THUMB_DOWN_COMMENT,  
+        id = id,
+        thumbNumber = votes
+    }         
 }
 const boundSubThumb = id => dispatch(subThumb(id));
-boundSubThumb(5);
+//boundSubThumb(5);
--- reducer.js
+++ reducer.js
@@ -1,47 +1,6 @@
-import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions'
+import { combineReducers } from 'redux';
+import comments from './comments';
 
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    text: action.text,
-                    votes: 0
-                }
-                , ...state]
-            })
-        default:
-            return state;
-        case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            });
-        default:
-            return state;
-        case EDIT_COMMENT:
-            return Object.assign({}, state, {
-                ...//wpisz tu coś
-            });
-        default:
-            return state;
-        case THUMB_UP_COMMENT:
-            return Object.assign({}, state, {
-                ...//wpisz tu coś
-            });
-        default:
-            return state;
-        case THUMB_DOWN_COMMENT:
-            return Object.assign({}, state, {
-                ...//wpisz tu coś
-            });
-        default:
-            return state;
-    
-}
-
-const initialState = {
-    comments: [],
-    users: []
-};
+const app = combineReducers({
+    comments
+});
