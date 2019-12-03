285164c70a65ad0eaad0f467f59a5df5e8b5de81
Karolina Kuchta <linka_94@interia.eu>
2019-12-03 10:28:20 +0100

    Change comments.js

--- comments.js
+++ comments.js
@@ -10,21 +10,21 @@ function comments(state = [], action) {
             }
             , ...state];
         case REMOVE_COMMENT:
-            return state.comments.filter(comment => comment.id !== action.id);
+            return state.comments.map(comment => comment.id !== action.id);
         case EDIT_COMMENT:
-            return state.comments.filter(comment => {
+            return state.comments.map(comment => {
                         if(comment.id === action.id) {
                             comment.text = action.text;
                         }
                     });
         case THUMB_UP_COMMENT:
-            return state.comments.filter(comment => {
+            return state.comments.map(comment => {
                         if(comment.id === action.id) {
                             comment.votes +=1;
                         }
                     });
         case THUMB_DOWN_COMMENT:
-            return state.comments.filter(comment => {
+            return state.comments.map(comment => {
                         if(comment.id === action.id) {
                             comment.votes -=1;
                         }
